136adf1a11ba081922fa29222a9517724c93d881
akash0525 <jaiananthi2004@gmail.com>
2025-12-31 09:29:24 +0530

    path modules

--- core-modules/path.js
+++ core-modules/path.js
@@ -3,4 +3,10 @@ const path = require("path");
 
 const filePath = "Users/Akash/Desktop/node-js-practice/intro/app.js";
 
-console.log("base name:",path.basename(filePath))
+console.log("base name:",path.basename(filePath));
+console.log("directory name", path.dirname(filePath));
+console.log("extension name",path.extname(filePath));
+
+
+//test.txt  path
+console.log('joinedPath',__dirname+'\\test.txt');
